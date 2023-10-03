8db4431428d55f0f29cb715777e08ad47d1e390f
Kordianek <145443055+Kordianek@users.noreply.github.com>
2023-10-03 10:07:16 +0200

    zadanie3.php

--- zadanie3.php
+++ zadanie3.php
@@ -1,16 +1,18 @@
 <?php
-$liczba = -4;
-$n = 0;
-
-for($i = 2; $i<=$liczba; $i++) {
-  if($liczba % $i == 0){
-    $n++;
-    break;
-  }
+function lpierwsza($liczba){
+    if ($liczba<=1)
+    return false;
+     
+    for ($i = 2; $i <= sqrt($liczba); $i++){
+        if ($liczba % $i == 0)
+            return false;
+    }
+    return true;
 }
-if ($n == 0 && $liczba<=0){
-  echo $liczba." jest liczbą pierwszą.";
-} else {
-  echo $liczba." nie jest liczbą pierwszą.";
-}
+$liczba = 7;
+$lp = lpierwsza($liczba);
+if ($lp == 1)
+    echo $liczba." jest pierwsza";
+else
+    echo $liczba." nie jest pierwsza";
 ?>
